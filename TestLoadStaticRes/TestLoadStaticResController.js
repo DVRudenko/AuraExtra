0a1c2a96dfdabdbb36ba410152afd1d5ae5cd8e0
Dmitry Rudenko <51669429+DVRudenko@users.noreply.github.com>
2021-12-14 00:03:04 +0300

    Add files via upload

---
+++ TestLoadStaticRes/TestLoadStaticResController.js
@@ -0,0 +1,9 @@
+({
+  getValue : function(component, event, helper) {
+    component.set("v.value", _counter.getValue());
+  },
+
+  increment : function(component, event, helper) {
+    component.set("v.value", _counter.increment());
+  }
+})
